8edcc6d0ea36cf012e8a6e43433812160b6a1c5c
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-07-05 07:49:10 +0100

    added the api endpoint route for signing up users

--- env.js
+++ env.js
@@ -6,7 +6,7 @@ export default {
   database_url: process.env.DATABASE_URL,
   test_database_url: process.env.TEST_DATABASE_URL,
   secret: process.env.SECRET,
-  port: process.env.PORT || 7000,
+  port: process.env.PORT || 5000,
   environment: process.env.NODE_ENV,
 
 }
