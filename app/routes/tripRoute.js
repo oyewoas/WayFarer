f59831fba0a6790b3f0bec5b7bdefe4cabe23164
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-07-10 07:37:34 +0100

    User can get and filter trips using trip origin.

--- app/routes/tripRoute.js
+++ app/routes/tripRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createTrip, getAllTrips, cancelTrip } from '../controllers/tripController';
+import { createTrip, getAllTrips, cancelTrip, filterTripByOrigin } from '../controllers/tripController';
 import verifyAuth from '../middlewares/verifyAuth';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 // trips Routes
 
 router.post('/trips', verifyAuth, createTrip);
-router.get('/trips', getAllTrips);
+router.get('/trips', verifyAuth, getAllTrips);
 router.patch('/trips/:tripId', verifyAuth, cancelTrip);
+router.get('/trip', verifyAuth, filterTripByOrigin);
 export default router;
